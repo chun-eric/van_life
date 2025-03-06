9e2a6b6c08c17d3e76372687cb4237f4ea3e221a
Eric Chun <eric.chun88800888@gmail.com>
2025-03-06 17:31:59 +0900

    fixed search functionality

--- src/component/RentalDatePicker.jsx
+++ src/component/RentalDatePicker.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 const RentalDatePicker = ({
   pickupDate,
@@ -8,79 +8,79 @@ const RentalDatePicker = ({
   onPickupDateChange,
   onPickupTimeChange,
   onReturnDateChange,
-  onReturnTimeChange,
+  onReturnTimeChange
 }) => {
   return (
-    <div className='flex flex-col md:flex-row w-full border border-slate-950 divide-y md:divide-y-0 md:divide-x divide-slate-950'>
-      {" "}
+    <div className='flex flex-col w-full border divide-y md:flex-row border-slate-950 md:divide-y-0 md:divide-x divide-slate-950'>
+      {' '}
       {/* Pick up section */}
-      <div className='flex-1 flex  '>
-        <div className='flex-1 flex flex-col gap-2  p-2 border-r border-slate-950 items-start'>
-          <label className='block text-xs text-gray-700 mb-1'>
+      <div className='flex flex-1 w-full'>
+        <div className='flex flex-col items-start flex-1 w-full gap-2 p-2 border-r border-slate-950'>
+          <label className='block mb-1 text-xs text-gray-700'>
             Pickup Date
           </label>
-          <div className='flex items-center pr-2 '>
+          <div className='flex items-center w-full pr-2 '>
             <div className='w-full '>
               <input
                 type='date'
-                className='w-full text-sm bg-gray-100 px-2 py-1'
+                className='w-full px-2 py-1 text-sm bg-gray-100'
                 value={pickupDate}
-                onChange={(e) => onPickupDateChange(e.target.value)}
+                onChange={e => onPickupDateChange(e.target.value)}
               />
             </div>
           </div>
         </div>
-        <div className='flex-1 flex flex-col gap-2 p-2 items-start'>
-          <label className='block text-xs text-gray-700 mb-1'>
+        <div className='flex flex-col items-start flex-1 w-full gap-2 p-2'>
+          <label className='block w-full mb-1 text-xs text-gray-700'>
             Pickup Time
           </label>
-          <div className='flex items-center  '>
+          <div className='flex items-center w-full '>
             <div className='w-full '>
               <input
                 type='time'
-                className='w-full text-sm bg-gray-100 px-2 py-1'
+                className='w-full px-2 py-1 text-sm bg-gray-100'
                 value={pickupTime}
-                onChange={(e) => onPickupTimeChange(e.target.value)}
+                onChange={e => onPickupTimeChange(e.target.value)}
               />
             </div>
           </div>
         </div>
       </div>
       {/* Return section */}
-      <div className='flex-1 flex  '>
-        <div className='flex-1 flex flex-col gap-2 p-2 border-r border-slate-950 items-start'>
-          <label className='block text-xs text-gray-700 mb-1'>
+      <div className='flex flex-1 '>
+        <div className='flex flex-col items-start justify-center flex-1 gap-2 p-2 border-r border-slate-950'>
+          <label className='block mb-1 text-xs text-gray-700'>
             Return Date
           </label>
-          <div className='flex items-center pr-2 '>
+          <div className='flex items-center w-full pr-2 '>
             <div className='w-full '>
               <input
                 type='date'
-                className='w-full text-sm bg-gray-100 px-2 py-1'
+                className='w-full px-2 py-1 text-sm bg-gray-100'
                 value={returnDate}
-                onChange={(e) => onReturnDateChange(e.target.value)}
+                onChange={e => onReturnDateChange(e.target.value)}
               />
             </div>
           </div>
         </div>
-        <div className='flex-1 flex flex-col gap-2 p-2 items-start'>
-          <label className='block text-xs text-gray-700 mb-1'>
+        <div className='flex flex-col items-start justify-center flex-1 gap-2 p-2'>
+          <label className='block mb-1 text-xs text-gray-700'>
             Return Time
           </label>
-          <div className='flex items-center  '>
+          <div className='flex items-center w-full'>
             <div className='w-full '>
               <input
                 type='time'
-                className='w-full text-sm bg-gray-100 px-2 py-1'
+                className='w-full px-2 py-1 text-sm bg-gray-100'
                 value={returnTime}
-                onChange={(e) => onReturnTimeChange(e.target.value)}
+                onChange={e => onReturnTimeChange(e.target.value)}
               />
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default RentalDatePicker;
+export default RentalDatePicker
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,123 +1,123 @@
-import { useState, useEffect } from "react";
-import { Link, useSearchParams } from "react-router-dom";
-import "../../server.js";
-import { getVans } from "../../api.js";
-import VanCard from "./VanCard.jsx";
-import SearchBar from "../../component/SearchBar.jsx";
-import SortSelect from "../../component/SortSelect.jsx";
+import { useState, useEffect } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
+import '../../server.js'
+import { getVans } from '../../api.js'
+import VanCard from './VanCard.jsx'
+import SearchBar from '../../component/SearchBar.jsx'
+import SortSelect from '../../component/SortSelect.jsx'
 // import DateRangePicker from "../../component/DateRangePicker.jsx";
-import RentalDatePicker from "../../component/RentalDatePicker.jsx";
+import RentalDatePicker from '../../component/RentalDatePicker.jsx'
 
 const Vans = () => {
   // get current time rounded to nearest half hour in HH:MM format
   const getCurrentTime = () => {
-    const now = new Date();
-    console.log("now", now);
+    const now = new Date()
+    console.log('now', now)
 
-    let hours = now.getHours();
-    const minutes = now.getMinutes();
-    const roundedMinutes = Math.round(minutes / 30) * 30; // round minutes to nearest 30
+    let hours = now.getHours()
+    const minutes = now.getMinutes()
+    const roundedMinutes = Math.round(minutes / 30) * 30 // round minutes to nearest 30
 
     // handle hour rollover
     if (roundedMinutes === 60) {
-      hours = (hours + 1) % 24;
-      return `${hours.toString().padStart(2, "0")}:00`;
+      hours = (hours + 1) % 24
+      return `${hours.toString().padStart(2, '0')}:00`
     }
 
-    const formattedHours = hours.toString().padStart(2, "0");
-    const formattedMinutes = roundedMinutes.toString().padStart(2, "0");
-    return `${formattedHours}:${formattedMinutes}`;
-  };
+    const formattedHours = hours.toString().padStart(2, '0')
+    const formattedMinutes = roundedMinutes.toString().padStart(2, '0')
+    return `${formattedHours}:${formattedMinutes}`
+  }
 
   // get current date
-  const today = new Date().toISOString().split("T")[0];
+  const today = new Date().toISOString().split('T')[0]
   // set initial pickup and return times
-  const initialPickupTime = getCurrentTime();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  const initialReturnDateFormatted = tomorrow.toISOString().split("T")[0];
+  const initialPickupTime = getCurrentTime()
+  const tomorrow = new Date(today)
+  tomorrow.setDate(tomorrow.getDate() + 1)
+  const initialReturnDateFormatted = tomorrow.toISOString().split('T')[0]
 
-  const [vans, setVans] = useState([]); // all vans state
-  const [searchParams, setSearchParams] = useSearchParams(); // URL search params
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [sort, setSort] = useState("");
-  const [pickupDate, setPickupDate] = useState(today);
-  const [pickupTime, setPickupTime] = useState(initialPickupTime);
-  const [returnDate, setReturnDate] = useState(initialReturnDateFormatted);
-  const [returnTime, setReturnTime] = useState(initialPickupTime);
+  const [vans, setVans] = useState([]) // all vans state
+  const [searchParams, setSearchParams] = useSearchParams() // URL search params
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [searchQuery, setSearchQuery] = useState('')
+  const [sort, setSort] = useState('')
+  const [pickupDate, setPickupDate] = useState(today)
+  const [pickupTime, setPickupTime] = useState(initialPickupTime)
+  const [returnDate, setReturnDate] = useState(initialReturnDateFormatted)
+  const [returnTime, setReturnTime] = useState(initialPickupTime)
 
-  const typeFilter = searchParams.get("type"); // URL search type
-  console.log(typeFilter);
+  const typeFilter = searchParams.get('type') // URL search type
+  console.log(typeFilter)
 
   // sort all vans first
   const sortVans = [...vans].sort((a, b) => {
-    if (sort === "low-to-high") {
-      return a.price - b.price;
-    } else if (sort === "high-to-low") {
-      return b.price - a.price;
+    if (sort === 'low-to-high') {
+      return a.price - b.price
+    } else if (sort === 'high-to-low') {
+      return b.price - a.price
     }
-    return 0;
-  });
+    return 0
+  })
 
   // Filtered vans array based on url search type
   const filteredVans = typeFilter
-    ? sortVans.filter((van) => {
-        const matchesType = van.type === typeFilter;
+    ? sortVans.filter(van => {
+        const matchesType = van.type === typeFilter
         const matchesSearch = van.name
           .toLowerCase()
-          .includes(searchQuery.toLowerCase());
-        return matchesType && matchesSearch;
+          .includes(searchQuery.toLowerCase())
+        return matchesType && matchesSearch
       })
-    : sortVans.filter((van) =>
+    : sortVans.filter(van =>
         van.name.toLowerCase().includes(searchQuery.toLowerCase())
-      );
+      )
 
   useEffect(() => {
-    async function loadVans() {
-      setLoading(true);
-      setError(null);
+    async function loadVans () {
+      setLoading(true)
+      setError(null)
       try {
-        const data = await getVans();
-        setVans(data);
+        const data = await getVans()
+        setVans(data)
       } catch (error) {
-        setError(error);
-        setVans([]);
+        setError(error)
+        setVans([])
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
     }
-    loadVans();
-  }, []);
+    loadVans()
+  }, [])
 
   // color options
   const colorOptions = {
-    simple: "#e17653",
-    rugged: "#115E59",
-    luxury: "#161616",
-  };
+    simple: '#e17653',
+    rugged: '#115E59',
+    luxury: '#161616'
+  }
 
-  function handleFilterChange(key, value) {
-    setSearchParams((prevParams) => {
+  function handleFilterChange (key, value) {
+    setSearchParams(prevParams => {
       if (value === null) {
-        prevParams.delete(key);
+        prevParams.delete(key)
       } else {
-        prevParams.set(key, value);
+        prevParams.set(key, value)
       }
 
-      return prevParams;
-    });
+      return prevParams
+    })
   }
 
-  function handleSearch(value) {
-    console.log("Value received from SearchBar:", value);
-    setSearchQuery(value);
+  function handleSearch (value) {
+    console.log('Value received from SearchBar:', value)
+    setSearchQuery(value)
   }
 
-  function handleSort(value) {
-    console.log("Value received from SortSelect:", value);
-    setSort(value);
+  function handleSort (value) {
+    console.log('Value received from SortSelect:', value)
+    setSort(value)
   }
 
   // if loading
@@ -130,7 +130,7 @@ const Vans = () => {
           </h1>
         </div>
       </div>
-    );
+    )
   }
 
   // if error occurs
@@ -148,27 +148,27 @@ const Vans = () => {
           </div>
         </div>
       </div>
-    );
+    )
   }
 
   return (
-    <div className='w-full px-8 py-24 bg-white sm:px-6 border '>
+    <div className='w-full px-8 py-24 bg-white border sm:px-6 '>
       <div className=' max-w-[1280px] mx-auto '>
         <h1 className='mb-3 text-xl font-bold md:text-4xl'>Explore Our Vans</h1>
         <p className='my-2 mb-6 text-xs'>
           Find the perfect van for your next adventure!
         </p>
-        <div className='flex flex-col mb-6  '>
+        <div className='flex flex-col mb-6 '>
           {/* Container for all three components with responsive layouts */}
-          <div className='flex flex-col space-y-1 lg:space-y-0 lg:flex-row lg:items-stretch  '>
+          <div className='flex flex-col space-y-1 lg:space-y-0 lg:flex-row lg:items-stretch '>
             {/* Search Bar - Full width on all screens */}
-            <div className='w-full  flex-1 '>
+            <div className='flex-1 w-full lg:max-w-[400px]'>
               <SearchBar onSearch={handleSearch} />
-              {console.log("Current search query:", searchQuery)}
+              {console.log('Current search query:', searchQuery)}
             </div>
 
             {/* Container for DateRange and Sort - Stack on mobile, row on tablet, inline on desktop */}
-            <div className='w-full  flex-1 '>
+            <div className='flex-1 w-full '>
               {/* <DateRangePicker /> */}
               <RentalDatePicker
                 pickupDate={pickupDate}
@@ -188,17 +188,17 @@ const Vans = () => {
         </div>
         <div className='flex flex-col flex-wrap items-center justify-between mb-4 xs:flex-row'>
           <div className='flex flex-wrap w-full gap-1 xs:w-auto'>
-            {Object.keys(colorOptions).map((type) => (
+            {Object.keys(colorOptions).map(type => (
               <button
                 key={type}
                 className='no-underlines'
-                onClick={() => handleFilterChange("type", type)}
+                onClick={() => handleFilterChange('type', type)}
               >
                 <span
                   style={{
                     backgroundColor:
-                      typeFilter === type ? colorOptions[type] : "#ffead0",
-                    color: typeFilter === type ? "white" : "black",
+                      typeFilter === type ? colorOptions[type] : '#ffead0',
+                    color: typeFilter === type ? 'white' : 'black'
                   }}
                   className={`inline-block px-2 py-2 capitalize max-w-[100px] text-center rounded mr-2 cursor-pointer text-xs`}
                 >
@@ -209,7 +209,7 @@ const Vans = () => {
           </div>
           {typeFilter && (
             <button
-              onClick={() => handleFilterChange("type", null)}
+              onClick={() => handleFilterChange('type', null)}
               className='flex w-full mt-4 ml-2 xs:ml-0 xs:w-auto xs:mt-0'
             >
               <span
@@ -222,7 +222,7 @@ const Vans = () => {
           )}
         </div>
         <div className='grid w-full grid-cols-1 gap-6 sm:gap-8 md:grid-cols-2 lg:grid-cols-4'>
-          {filteredVans.map((van) => (
+          {filteredVans.map(van => (
             // Van Card
             <VanCard
               key={van.id}
@@ -234,7 +234,7 @@ const Vans = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Vans;
+export default Vans
