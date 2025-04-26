7cc13f9e67aead154b2a6c6fa4e9be63e904d4ea
Eric Chun <eric.chun88800888@gmail.com>
2025-04-26 09:38:49 +0900

    Complete form validation

--- src/pages/Vans/Booking.jsx
+++ src/pages/Vans/Booking.jsx
@@ -14,6 +14,7 @@ const Booking = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors }
   } = useForm()
 
@@ -53,133 +54,166 @@ const Booking = () => {
     ' px-6 py-3 font-semibold text-black transition-all border border-black shadow-inner cursor-pointer hover:-translate-y-0.5 hover:shadow-md duration-300 ease-in-out'
 
   return (
-    <div className='flex flex-col item-center justify-center max-w-5xl h-[60%] gap-6 p-6 mx-auto md:flex-row'>
-      {/* Van Details Section */}
-      <div className='h-full p-4 rounded-lg shadow bg-gray-50 md:w-1/2'>
-        <div className='h-[50%]'>
-          {/* Main Image */}
-          <img
-            src={vanDetails.imageUrl[selectedImage]}
-            alt=''
-            className='w-full h-full mb-4 rounded shadow-sm'
-          />
-          {/* Thumbnail Images */}
-          <div className='flex gap-1 mt-1'>
-            {vanDetails.imageUrl?.map((img, index) => (
-              <button
-                key={index}
-                onClick={() => setSelectedImage(index)}
-                className={`w-24 h-24 overflow-hidden rounded ${
-                  selectedImage === index ? 'ring-2 ring-[#FF8C38]' : ''
-                }`}
-              >
-                <img
-                  src={img}
-                  alt={`Van thumbnail ${index + 1}`}
-                  className='object-cover w-full h-full'
-                />
-              </button>
-            ))}
+    <div className='min-h-screen'>
+      <div className='flex flex-col justify-center max-w-4xl gap-6 p-6 mx-auto item-center md:flex-row  h-[700px]'>
+        {/* Van Details Section */}
+        <div className='h-full p-4 rounded-lg shadow bg-gray-50 md:w-1/2 '>
+          <div className='flex flex-col '>
+            {/* Main Image */}
+            <img
+              src={vanDetails.imageUrl[selectedImage]}
+              alt=''
+              className='object-cover w-full mb-4 rounded shadow-sm h-[250px]'
+            />
+            {/* Thumbnail Images */}
+            <div className='flex w-full gap-1 mt-1'>
+              {vanDetails.imageUrl?.map((img, index) => (
+                <button
+                  key={index}
+                  onClick={() => setSelectedImage(index)}
+                  className={`w-20 h-20 overflow-hidden rounded ${
+                    selectedImage === index ? 'ring-2 ring-[#FF8C38]' : ''
+                  }`}
+                >
+                  <img
+                    src={img}
+                    alt={`Van thumbnail ${index + 1}`}
+                    className='object-cover w-full h-full'
+                  />
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className='flex flex-col w-full gap-3'>
+            <h3 className='mt-3 text-2xl font-bold'>{vanDetails.name}</h3>
+            <p className='mb-4 text-sm leading-relaxed text-slate-600'>
+              {vanDetails.description}
+            </p>
+            <p className='text-lg font-bold'>${vanDetails.price} a day</p>
           </div>
         </div>
-        <div className='flex flex-col w-full gap-3 h-[50%]'>
-          <h3 className='mt-3 text-2xl font-bold'>{vanDetails.name}</h3>
-          <p className='mb-4 text-sm leading-relaxed text-slate-600'>
-            {vanDetails.description}
-          </p>
-          <p className='text-lg font-bold'>${vanDetails.price} a day</p>
+
+        {/* Booking Form Section */}
+        <div className='h-full p-6 bg-white border border-black rounded-lg shadow md:w-1/2'>
+          <h1 className='mb-4 text-xl font-bold'>Booking Request</h1>
+
+          <form onSubmit={handleSubmit(onSubmit)} className='space-y-6'>
+            {/* Pick up date */}
+            <div className=''>
+              <label className='block mb-2 text-sm text-gray-700'>
+                Pickup Date
+              </label>
+              <input
+                type='date'
+                {...register('pickupDate', {
+                  required: 'Pickup date is required'
+                })}
+                className='w-full px-3 py-2 text-sm text-gray-700 border rounded outline-none'
+              />
+              {errors.pickupDate && (
+                <p className='text-sm text-red-500'>
+                  {errors.pickupDate.message}
+                </p>
+              )}
+            </div>
+
+            {/* Pick up date */}
+            <div className=''>
+              <label className='block mb-2 text-sm text-gray-700'>
+                Pickup Time
+              </label>
+              <input
+                type='time'
+                {...register('pickupTime', {
+                  required: 'Pickup time is required',
+                  validate: value => {
+                    const [hours, minutes] = value.split(':').map(Number)
+                    if (hours < 14) {
+                      return 'Earliest pickup time is 2:00pm'
+                    }
+                    return true
+                  }
+                })}
+                className='w-full px-3 py-2 text-sm text-gray-700 border rounded'
+              />
+              {errors.pickupTime && (
+                <p className='text-sm text-red-500'>
+                  {errors.pickupTime.message}
+                </p>
+              )}
+            </div>
+
+            {/* Drop-off Date */}
+            <div className=''>
+              <label className='block mb-2 text-sm text-gray-700'>
+                Drop-off Date
+              </label>
+              <input
+                type='date'
+                className='w-full px-3 py-2 text-sm text-gray-700 border rounded'
+                {...register('dropoffDate', {
+                  // Use 'dropoffDate' consistently
+                  required: 'Drop-off date is required',
+                  validate: value => {
+                    const pickupDate = watch('pickupDate')
+                    if (!pickupDate) {
+                      return 'Please select a pickup date first'
+                    }
+                    const pickup = new Date(pickupDate)
+                    const dropoff = new Date(value)
+
+                    // Ensure drop off date is at least one day after pickup date
+                    const diffInTime = dropoff.getTime() - pickup.getTime() // gets the difference in date in millisecons
+                    const diffInDays = diffInTime / (1000 * 60 * 60 * 24) // convert milliseconds in days
+
+                    if (diffInDays < 1) {
+                      return 'Drop-off date must be at least one day after the pickup date'
+                    }
+                    return true
+                  }
+                })}
+              />
+              {errors.dropoffDate && (
+                <p className='text-sm text-red-500'>
+                  {errors.dropoffDate.message}
+                </p>
+              )}
+            </div>
+
+            {/* Drop-off Time */}
+            <div className=''>
+              <label className='block mb-2 text-sm text-gray-700'>
+                Drop-off Time
+              </label>
+              <input
+                type='time'
+                className='w-full px-3 py-2 text-sm text-gray-700 border rounded'
+                {...register('dropoffTime', {
+                  required: 'Drop-off time is required',
+                  validate: value => {
+                    const [hours, minutes] = value.split(':').map(Number)
+                    if (hours > 12 || (hours === 12 && minutes > 0)) {
+                      return 'Latest drop-off time is 12:00pm'
+                    }
+                  }
+                })}
+              />
+              {errors.dropoffTime && (
+                <p className='text-sm text-red-500'>
+                  {errors.dropoffTime.message}
+                </p>
+              )}
+            </div>
+
+            {/* Submit Button */}
+            <button
+              type='submit'
+              className={`${buttonBaseClasses} bg-[#FDBA74] hover:bg-orange-50 font-bold `}
+            >
+              Booking Confirmation
+            </button>
+          </form>
         </div>
-      </div>
-
-      {/* Booking Form Section */}
-      <div className='p-6 bg-white border border-black rounded-lg shadow md:w-1/2'>
-        <h1 className='mb-4 text-xl font-bold'>Booking Request</h1>
-
-        <form onSubmit={handleSubmit(onSubmit)} className='space-y-6'>
-          {/* Pick up date */}
-          <div className=''>
-            <label className='block mb-2 text-sm text-gray-700'>
-              Pickup Date
-            </label>
-            <input
-              type='date'
-              {...register('pickupDate', {
-                required: 'Pickup date is required'
-              })}
-              className='w-full px-3 py-2 text-sm text-gray-700 border rounded outline-none'
-            />
-            {errors.pickupDate && (
-              <p className='text-sm text-red-500'>
-                {errors.pickupDate.message}
-              </p>
-            )}
-          </div>
-
-          {/* Pick up date */}
-          <div className=''>
-            <label className='block mb-2 text-sm text-gray-700'>
-              Pickup Time
-            </label>
-            <input
-              type='time'
-              {...register('pickupTime', {
-                required: 'Pickup time is required'
-              })}
-              className='w-full px-3 py-2 text-sm text-gray-700 border rounded'
-            />
-            {errors.pickupTime && (
-              <p className='text-sm text-red-500'>
-                {errors.pickupTime.message}
-              </p>
-            )}
-          </div>
-
-          {/* Drop-off Date */}
-          <div className=''>
-            <label className='block mb-2 text-sm text-gray-700'>
-              Drop-off Date
-            </label>
-            <input
-              type='date'
-              className='w-full px-3 py-2 text-sm text-gray-700 border rounded'
-              {...register('dropoffdate', {
-                required: 'Drop-off date is required'
-              })}
-            />
-            {errors.dropoffDate && (
-              <p className='text-sm text-red-500'>
-                {errors.dropoffDate.message}
-              </p>
-            )}
-          </div>
-
-          {/* Drop-off Time */}
-          <div className=''>
-            <label className='block mb-2 text-sm text-gray-700'>
-              Drop-off Time
-            </label>
-            <input
-              type='time'
-              className='w-full px-3 py-2 text-sm text-gray-700 border rounded'
-              {...register('dropofftime', {
-                required: 'Drop-off time is required'
-              })}
-            />
-            {errors.dropoffTime && (
-              <p className='text-sm text-red-500'>
-                {errors.dropoffTime.message}
-              </p>
-            )}
-          </div>
-
-          {/* Submit Button */}
-          <button
-            type='submit'
-            className={`${buttonBaseClasses} bg-[#FDBA74] hover:bg-orange-50 font-bold `}
-          >
-            Booking Confirmation
-          </button>
-        </form>
       </div>
     </div>
   )
